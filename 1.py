3d819d345b3675991a049782c5bc453ddd018420
GilfoyleSV <80770574+GilfoyleSV@users.noreply.github.com>
2023-01-10 05:16:51 +0500

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,16 +1,13 @@
-x = input('Введите число:')
-lenght = len(x)
-index_first_num = 0
+def get_max(x, y, z):
+    max = 0
+    if x > y:
+        max = x
+    else:
+        max = y
 
-if float(x) < 1:
+    if max > z:
+        pass
+    else:
+        max = z
 
-    for i in x:
-
-        if i in '123456789':
-            index_first_num = x.index(i)
-            break
-
-    print("Формат плавающей точки: " + x[index_first_num] + '.' + x[index_first_num + 1: lenght] + f' * 10 ** {-(index_first_num - 1)}')
-
-else:
-    print('Формат плавающей точки: ' + x[0] + '.' + x[1:] + f' * 10 ** {lenght - 1}')
+    return max
